031bba7f324414771bc0e79d781e158696988039
Loc Nguyen <vinhloc30796@gmail.com>
2022-06-24 16:28:08 +0700

    Add fn_hello_extended

--- 2.lesson/hands-on/src/functions.rs
+++ 2.lesson/hands-on/src/functions.rs
@@ -11,3 +11,9 @@ fn function_name() -> return_type {
 pub fn fn_hello() -> &'static str {
     "Hello from Barcelona"
 }
+
+#[allow(dead_code)]
+pub fn fn_hello_extended(place: Option<String>) -> String {
+    let prefix = "Hello from ";
+    prefix.to_owned() + &place.unwrap_or("Vietnam".to_string())
+}
