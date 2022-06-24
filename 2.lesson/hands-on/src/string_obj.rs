36f3a4f4e92c836e15833e8b63bf200fec4b57a4
Loc Nguyen <vinhloc30796@gmail.com>
2022-06-24 16:15:08 +0700

    Minor modifications to test things out

--- 2.lesson/hands-on/src/string_obj.rs
+++ 2.lesson/hands-on/src/string_obj.rs
@@ -3,6 +3,7 @@ pub fn print_string_obj() {
     let empty_string = String::new();
     println!("length is {}", empty_string.len());
 
-    let content_string = String::from("AckeeBlockchain");
-    println!("length is {}", content_string.len());
+    // let content_string = String::from("AckeeBlockchain");
+    let content_string: &str = "AckeeBlockchain";
+    println!("length of {} is {}", content_string, content_string.len());
 }
